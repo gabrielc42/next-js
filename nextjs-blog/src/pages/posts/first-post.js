593c276dfff48ed50a9eca9d3c2d86440c597685
gabrielc42 <clark.gabe97@gmail.com>
2023-04-12 21:49:13 -0500

    polishing layout

--- nextjs-blog/src/pages/posts/first-post.js
+++ nextjs-blog/src/pages/posts/first-post.js
@@ -1,17 +1,23 @@
 import Head from 'next/head';
 import Link from 'next/link';
-
+import Script from 'next/script';
+import Layout from '/components/layout';
 
 export default function FirstPost() {
   return (
     <>
-      <Head>
-        <title>First Post</title>
-      </Head>
-      <h1>First Post</h1>
-      <h2>
-        <Link href="/">← Back to home</Link>
-      </h2>
+      <Layout>
+        <Head>
+          <title>First Post</title>
+        </Head>
+        <Script
+          src="https://connect.facebook.net/en_US/sdk.js"
+          strategy="lazyOnload"
+          onLoad={() =>
+            console.log(`script loaded correctly, window.FB has been populated`)
+          }
+        />
+      </Layout>
     </>
   );
 }
