debc022c7b7fcaf645350adcf79c4a4ea04549c5
gabrielc42 <clark.gabe97@gmail.com>
2023-05-19 13:46:30 -0500

    handling form input w// API routes notes

--- nextjs-blog/src/pages/api/hello.js
+++ nextjs-blog/src/pages/api/hello.js
@@ -1,3 +1,12 @@
 export default function handler(req, res) {
     res.status(200).json({ text: 'Hello' });
 }
+
+// handling form input w/ API routes
+// save to database, api route code will not be part of client bundle, so can safely write server-side code
+
+// export default function handler(req, res) {
+//     const email = req.body.email;
+//     // Then save email to your database, etc...
+//   }
+  
